1c9bcd9beb0f1db8a2faad847aa9c5e2b6077dbe
mohammed zato <zatomohammed231@gmail.com>
2025-08-16 14:18:52 +0000

    created role based middleware

---
+++ Middleware/authorizeRoles.js
@@ -0,0 +1,13 @@
+const authorizedRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      res.status(403).json({
+        status: "Forbidden",
+        message: `Role ${req.user.role} is not authorized to access this resource`,
+      });
+    }
+    next();
+  };
+};
+
+module.exports = authorizedRoles;
--- Modules/Products/Controller/products.routes.js
+++ Modules/Products/Controller/products.routes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const authHandler = require("../../../Middleware/authHandler");
 const getSingleProduct = require("../getSingleProduct");
 const getAllProducts = require("../getAllProduct");
+const authorizedRoles = require("../../../Middleware/authorizeRoles");
 
 const productsRoutes = express.Router();
 
-productsRoutes.get("/:productId", getSingleProduct);
 productsRoutes.get("/", getAllProducts);
 
 productsRoutes.use(authHandler);
+
+productsRoutes.get("/:productId", getSingleProduct);
+
+productsRoutes.use(authorizedRoles);
 
 module.exports = productsRoutes;
