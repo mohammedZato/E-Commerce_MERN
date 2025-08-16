1f37eddcb977176cc958ababedca816aa32920e5
mohammed zato <zatomohammed231@gmail.com>
2025-08-16 12:03:31 +0000

    all product route added

--- Modules/Products/Controller/products.routes.js
+++ Modules/Products/Controller/products.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const authHandler = require("../../../Middleware/authHandler");
 const getSingleProduct = require("../getSingleProduct");
+const getAllProducts = require("../getAllProduct");
 
 const productsRoutes = express.Router();
 
 productsRoutes.use(authHandler);
 
 productsRoutes.get("/:productId", getSingleProduct);
+productsRoutes.get("/", getAllProducts);
 
 module.exports = productsRoutes;
