22556c15df8ccc5ce89dc86bc68566f7ab512e21
mohammed zato <zatomohammed231@gmail.com>
2025-08-18 02:25:16 +0000

    remove from cart route added

--- Modules/Cart Items/Controller/carts.routes.js
+++ Modules/Cart Items/Controller/carts.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const addToCart = require("../addToCart");
 const authHandler = require("../../../Middleware/authHandler");
+const removeFromCart = require("../removeFromCart");
 
 const cartsRoutes = express.Router();
 
 cartsRoutes.use(authHandler);
 
 cartsRoutes.post("/addtocart/:productId", addToCart);
+cartsRoutes.delete("/removefromcart/:cartitemId", removeFromCart);
 
 module.exports = cartsRoutes;
