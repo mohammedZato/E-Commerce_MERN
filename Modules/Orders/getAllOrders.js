5689b33a42809953f1e6836195b4abfe95b8e490
mohammed zato <zatomohammed231@gmail.com>
2025-08-20 23:43:22 +0000

    modified get all orders route

--- Modules/Orders/getAllOrders.js
+++ Modules/Orders/getAllOrders.js
@@ -3,16 +3,25 @@ const mongoose = require("mongoose");
 const getAllOrders = async (req, res) => {
   const ordersModel = mongoose.model("orders");
 
-  const getAllOrders = await ordersModel.findOne({
-    user: req.user._id,
-  });
-  if (!getAllOrders)
-    return res.status(500).json({ message: "You have no orders at this time" });
+  try {
+    const getAllOrders = await ordersModel.find({
+      user: req.user._id,
+    });
+    if (!getAllOrders)
+      return res
+        .status(500)
+        .json({ message: "You have no orders at this time" });
 
-  res.status(200).json({
-    status: "my orders",
-    getAllOrders,
-  });
+    res.status(200).json({
+      status: "my orders",
+      getAllOrders,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
 };
 
 module.exports = getAllOrders;
