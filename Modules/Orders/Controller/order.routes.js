7fdcdf8d8550992a349b12e1e50b3c652c1d612a
mohammed zato <zatomohammed231@gmail.com>
2025-08-21 11:50:06 +0000

    added admin only get all orders route

--- Modules/Orders/Controller/order.routes.js
+++ Modules/Orders/Controller/order.routes.js
@@ -3,6 +3,8 @@ const createOrder = require("../createOrder");
 const authHandler = require("../../../Middleware/authHandler");
 const getAllOrders = require("../getAllOrders");
 const getSingleOrder = require("../getSingleOrder");
+const allOrders = require("../adminGetAllOrders");
+const authorizedRoles = require("../../../Middleware/authorizeRoles");
 
 const orderRoutes = express.Router();
 
@@ -11,5 +13,9 @@ orderRoutes.use(authHandler);
 orderRoutes.post("/", createOrder);
 orderRoutes.get("/my-orders", getAllOrders);
 orderRoutes.get("/:orderId", getSingleOrder);
+
+orderRoutes.use(authorizedRoles("admin"));
+
+orderRoutes.get("/admin/allOrders", allOrders);
 
 module.exports = orderRoutes;
