6ab1816652f5147a1f44ae491d9ffbeafd688047
mohammed zato <zatomohammed231@gmail.com>
2025-08-21 16:30:34 +0000

    updated packages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./Handlers/errorHandler");
 const mongoose = require("mongoose");
 const userRoutes = require("./Modules/Users/Controller/users.route");
@@ -10,6 +11,7 @@ const cartsRoutes = require("./Modules/Cart Items/Controller/carts.routes");
 const orderRoutes = require("./Modules/Orders/Controller/order.routes");
 
 const app = express();
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -33,6 +35,12 @@ app.use("/api/products", productsRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/cart", cartsRoutes);
 app.use("/api/orders", orderRoutes);
+app.all("/", (req, res, next) => {
+  res.status(404).json({
+    status: "Failed",
+    message: "Error!! Page Not Found",
+  });
+});
 
 app.use(errorHandler);
 
